47a18883eea4c01d79dae743f31d761ce0f5274f
Rich Trott <rtrott@gmail.com>
2014-02-12 16:49:47 -0800

    check object returned by constructor because why not?

--- tests/jasmine/spec/ilios_ui_rte.spec.js
+++ tests/jasmine/spec/ilios_ui_rte.spec.js
@@ -1,5 +1,9 @@
 describe("ilios_ui_rte", function () {
-  describe("RichTextEditor()", function () {
-
+  describe("RichTextEditor() constructor", function () {
+    it("should return a RichTextEditor object", function () {
+      var el = document.createElement('div');
+      var editor = new ilios.ui.RichTextEditor(el);
+      expect(editor instanceof ilios.ui.RichTextEditor).toBe(true);
+    });
   });
 });
