659b27b7af035fab39da30ce1fb12c60a6508a07
Rich Trott <rtrott@gmail.com>
2014-02-17 20:09:11 -0800

    simplify repeated ternary expression

--- web/application/views/program/steward_dialog_support.js
+++ web/application/views/program/steward_dialog_support.js
@@ -222,7 +222,7 @@ ilios.pm.stewardTreeFinishedPopulation = function (type, args) {
 // @private
 ilios.pm.correctForStewardPlaceHolderNodes = function (rootNode) {
     var children = rootNode.children;
-    var len = (children != null) ? children.length : 0;
+    var len = children ? children.length : 0;
     var node = null;
     var model = null;
 
