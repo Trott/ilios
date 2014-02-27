f3395625ab7cd0e8c7f21b89aee0b6378cbdd64a
Rich Trott <rtrott@gmail.com>
2014-02-26 22:31:18 -0800

    add pending spec stub, depends on ilios_ui

--- tests/jasmine/spec/scripts/ilios_dom.spec.js
+++ tests/jasmine/spec/scripts/ilios_dom.spec.js
@@ -2,4 +2,8 @@ describe("ilios_dom", function () {
 	it("should create the ilios.dom namespace", function () {
 		expect(ilios.dom).toBeDefined();
 	});
+
+    xdescribe("buildDialogPanel()", function () {
+
+    });
 });
